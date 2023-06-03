d86f7015ff05a5025965bdb1780abe1d0e0f830d
aarontravass <aaron.travass@hotmail.com>
2023-06-03 02:17:14 -0400

    added redis

---
+++ src/index_redis.js
@@ -0,0 +1,47 @@
+import streamjson from "stream-json/streamers/StreamArray.js";
+import { createReadStream } from "fs";
+import { createClient } from "redis";
+
+const client = createClient();
+
+const reduce = (start_object, end_object) => {
+  const result = {};
+  result["session_id"] = start_object.id;
+  result["start_time"] = start_object.timestamp;
+  result["end_time"] = end_object.timestamp;
+  result["duration"] =
+    parseInt(end_object.timestamp) - parseInt(start_object.timestamp) + "s";
+  result["late"] =
+    (parseInt(end_object.timestamp) - parseInt(start_object.timestamp)) / 3600 >
+    24;
+  result["damaged"] = Boolean(end_object.comments).valueOf();
+  return result;
+};
+
+async function app() {
+  await client.connect();
+
+  const inputJsonPath = "./input.json";
+
+  const pipeline = createReadStream(inputJsonPath).pipe(
+    streamjson.withParser()
+  );
+
+  let lastobject = null;
+  pipeline.on("data", async (chunk) => {
+    if (!lastobject) {
+      lastobject = chunk.value;
+    } else {
+      const result = reduce(lastobject, chunk.value);
+      lastobject = null;
+      client.hSet(result['session_id'], result);
+    }
+  });
+
+  pipeline.on("end", async () => {
+    //console.log(data)
+    await client.quit();
+  });
+}
+
+app();
