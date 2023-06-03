e9e724cd1fe4023cef1379cc4e70a599f534a5c8
aarontravass <43901677+aarontravass@users.noreply.github.com>
2023-06-03 21:21:29 +0000

    added kubectl

--- src/index.js
+++ src/index.js
@@ -53,6 +53,12 @@ function app() {
   pipeline.on("close", () => {
     //console.log(data)
     console.timeEnd("timer_2");
+    const used = process.memoryUsage();
+    for (let key in used) {
+      console.log(
+        `${key} ${Math.round((used[key] / 1024 / 1024) * 100) / 100} MB`
+      );
+    }
     writer._close();
   });
 }
